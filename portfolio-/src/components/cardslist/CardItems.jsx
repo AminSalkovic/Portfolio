228e4b39f3fb38eaaa769aa6c3afaac72219ca8d
AminSalkovic <aminsalkovic2019@gmail.com>
2022-10-12 19:14:13 +0200

    portfolio

---
+++ portfolio-/src/components/cardslist/CardItems.jsx
@@ -0,0 +1,32 @@
+
+const CardItems=[
+  {  
+    title:'HTML',
+  },
+  {  
+    title:'CSS',
+  },
+  {  
+    title:'Java Script',
+  },
+  {  
+    title:'React',
+  },
+  {  
+    title:'JSX',
+  },
+  {  
+    title:'React Router Dom',
+  },
+  {  
+    title:'RESTS APIs',
+  },
+  {  
+    title:'GIT',
+  },
+  {  
+    title:'JSON',
+  },
+ 
+]
+export default CardItems;
